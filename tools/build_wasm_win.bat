f152ae330f7abdc842d2d169c82487c295c76a4c
Jens Fursund <jens@fursund.com>
2025-06-26 15:51:11 +0200

    revert assimp add more exporters

--- tools/build_wasm_win.bat
+++ tools/build_wasm_win.bat
@@ -1,7 +1,7 @@
 pushd %~dp0\..
 
 call emsdk\emsdk_env.bat
-call emcmake cmake -B build_wasm -G "Unix Makefiles" -DEMSCRIPTEN=1 -DCMAKE_MAKE_PROGRAM=mingw32-make -DCMAKE_BUILD_TYPE=%1 . || goto :error
+call emcmake cmake -B build_wasm -G "Unix Makefiles" -DEMSCRIPTEN=1 -DCMAKE_MAKE_PROGRAM=mingw32-make -DASSIMP_BUILD_ALL_EXPORTERS_BY_DEFAULT=1 -DASSIMP_BUILD_GLTF_EXPORTER=1-DCMAKE_BUILD_TYPE=%1 . || goto :error
 call emmake mingw32-make -C build_wasm || goto :error
 popd
 echo Build Succeeded.
